889c7a5545db61fa4379c7c074041536a7c57288
Ankur Gupta <rukna1000@gmail.com>
2018-09-10 00:33:56 -0400

    removed unnecessay statements

--- life.c
+++ life.c
@@ -15,32 +15,7 @@ int main(int argc, char **argv) {
 		return EXIT_FAILURE;
 	}
 
-	printf("%d %d %d %d %d %s\n", 
-			ROWS,
-			COLUMNS,
-			GENS,
-			DO_PRINT,
-			DO_PAUSE,
-			INPUT_FILENAME);
-
 	boardFromFile(INPUT_FILENAME);
-
-	char *inputFileName;
-	FILE *input;
-
-	/* Eventually, need to try to open the input file.
-	*/
-	input = fopen(inputFileName, "r");
-	if (!input) {
-		printf("Unable to open input file: %s\n", inputFileName);
-		return EXIT_FAILURE;
-	}
-
-
-	/*Once opened, you can read from the file one character at a time with fgetc().
-	 * You can read one line at a time using fgets().
-	 * You can read from standard input (the keyboard) with getchar().
-	*/
 
 	return EXIT_SUCCESS;
 }
