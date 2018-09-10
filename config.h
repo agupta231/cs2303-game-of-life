f713a1aae94d425190b6d6b9fb97668e1ddf24c7
Ankur Gupta <rukna1000@gmail.com>
2018-09-09 22:25:28 -0400

    Add global variables to config header file

--- config.h
+++ config.h
@@ -3,3 +3,18 @@
  *
  * @author: Ankur Gupta <agupta4@wpi.edu>
  */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+int ROWS;
+int COLUMNS;
+int GENS;
+int DO_PRINT;
+int DO_PAUSE;
+char *INPUT_FILENAME;
+
+// Function prototypes:
+int getUserOptions(int argc, char **argv);
+
+#endif
